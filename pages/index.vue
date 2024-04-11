40076351abc620c858378f53df28dd493b7bb3dc
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-12 04:35:09 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,38 @@
 <template>
   <NuxtLayout>
     <Button @click="setLocale('en')">{{ $t('welcome') }}</Button>
-  </NuxtLayout>
+
+
+    <div class="bg-white">
+    <!-- Header -->
+    <HeaderView />
+
+    <main>
+      <!-- Hero section -->
+      <div class="z-0 relative">
+        <HeroSection />
+      </div>
+
+      <!-- <Screenshot /> -->
+
+      <Container class="z-30 relative">
+        <FAQ />
+      </Container>
+    </main>
+  </div>
+
+
+</NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import { Button } from "@wingui/vue"
+import { ref } from 'vue'
+import Screenshot from '~/components/Screenshot.vue';
+import Footer from '@takasqr/tw-vue-ui/custom/general/CustomFooter.vue'
+import Container from '@takasqr/tw-vue-ui/basic/container/Container.vue'
+
+const mobileMenuOpen = ref(false)
 const { locale, setLocale } = useI18n()
 
 definePageMeta({
